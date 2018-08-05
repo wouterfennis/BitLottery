f2cf976d4e3e02c8c4e5567ef79f1efedf9aa0f3
Wouter Fennis <wouter_hilversum@hotmail.com>
2018-08-05 15:32:45 +0200

    Added LotteryTests

---
+++ New-Architecture/BitLottery/Bitlottery.Business.UnitTests/LotteryTests.cs
@@ -0,0 +1,78 @@
+using BitLottery.Business;
+using BitLottery.Models;
+using BitLottery.Business.RandomGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using BitLottery.Business.RandomWrapper;
+using System.Linq;
+
+namespace Bitlottery.Business.UnitTests
+{
+  [TestClass]
+  public class LotteryTests
+  {
+    private Mock<IRandomGenerator> _randomGeneratorMock { get; set; }
+    private Mock<IRandomWrapper> _randomWrapperMock { get; set; }
+    private Lottery _lottery { get; set; }
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      _randomGeneratorMock = new Mock<IRandomGenerator>(MockBehavior.Strict);
+      _randomWrapperMock = new Mock<IRandomWrapper>(MockBehavior.Strict);
+
+      _lottery = new Lottery(_randomGeneratorMock.Object, _randomWrapperMock.Object);
+    }
+
+    [TestMethod]
+    public async Task GenerateDrawAsync_ReturnsDrawAsync()
+    {
+      // Arrange
+      int expectedSeed = 1;
+      var expectedRandomNumbers = new List<int> { expectedSeed };
+      var expectedBallotNumber = 12345;
+
+      GenerationSettings actualGenerationSettings = null;
+      _randomGeneratorMock
+        .Setup(mock => mock.GenerateRandomNumbersAsync(It.IsAny<GenerationSettings>()))
+        .Callback((GenerationSettings generationSettings) =>
+        {
+          actualGenerationSettings = generationSettings;
+        })
+        .ReturnsAsync(expectedRandomNumbers);
+
+      _randomWrapperMock
+        .Setup(mock => mock.Seed(expectedSeed));
+
+      _randomWrapperMock
+        .Setup(mock => mock.Next(10000, 99999))
+        .Returns(expectedBallotNumber);
+
+      // Act
+      Draw draw = await _lottery.GenerateDrawAsync();
+
+      // Assert
+      draw.Should().NotBeNull();
+      draw.Ballots.Should().NotBeNull();
+      draw.Ballots.Should().HaveCount(100);
+      draw.Ballots.Should();
+
+      Ballot firstBallot = draw.Ballots.First();
+      firstBallot.Number.Should().Be(expectedBallotNumber);
+      firstBallot.SellDate.Should().BeNull();
+
+      draw.DrawDate.Should().BeNull();
+
+      actualGenerationSettings.Should().NotBeNull();
+      actualGenerationSettings.NumberOfIntegers.Should().Be(1);
+      actualGenerationSettings.MinimalIntValue.Should().Be(1000000);
+      actualGenerationSettings.MaximumIntValue.Should().Be(10000000);
+
+      _randomGeneratorMock.VerifyAll();
+      _randomWrapperMock.VerifyAll();
+    }
+  }
+}
