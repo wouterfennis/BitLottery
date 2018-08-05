fe1c4c200d712418a6f5fe719aa539908188a7de
Wouter Fennis <wouter_hilversum@hotmail.com>
2018-08-05 15:31:58 +0200

    Changed Lottery code so it is more mockable

--- New-Architecture/BitLottery/BitLottery.Business/Lottery.cs
+++ New-Architecture/BitLottery/BitLottery.Business/Lottery.cs
@@ -1,7 +1,10 @@
 using BitLottery.Business.RandomGenerator;
+using BitLottery.Business.RandomWrapper;
 using BitLottery.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +13,25 @@ namespace BitLottery.Business
   public class Lottery : ILottery
   {
     private readonly IRandomGenerator _randomGenerator;
+    private readonly IRandomWrapper _randomWrapper;
+    private int _numberOfBallotsInDraw;
+    private int _minimalLengthOfBallotNumber;
+    private int _maximumLengthOfBallotNumber;
 
-    public Lottery(IRandomGenerator randomGenerator)
+    public Lottery(IRandomGenerator randomGenerator, IRandomWrapper randomWrapper)
     {
       _randomGenerator = randomGenerator;
+      _randomWrapper = randomWrapper;
+
+      var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+      var appsettings = config.GetSection("appsettings");
+      _numberOfBallotsInDraw = Int32.Parse(appsettings["NumberOfBallotsInDraw"]);
+      _minimalLengthOfBallotNumber = Int32.Parse(appsettings["MinimalLengthOfBallotNumber"]);
+      _maximumLengthOfBallotNumber = Int32.Parse(appsettings["MaximumLengthOfBallotNumber"]);
     }
 
     public async Task<Draw> GenerateDrawAsync()
@@ -32,19 +50,19 @@ namespace BitLottery.Business
       var settings = new GenerationSettings
       {
         NumberOfIntegers = 1,
-        MinimalIntValue = 1000000,
-        MaximumIntValue = 10000000
+        MinimalIntValue = _minimalLengthOfBallotNumber,
+        MaximumIntValue = _maximumLengthOfBallotNumber
       };
 
       IEnumerable<int> randomNumbers = await _randomGenerator.GenerateRandomNumbersAsync(settings);
-      int seed = randomNumbers.First();
+      int seed = randomNumbers.Single();
 
-      var random = new Random(seed);
+      _randomWrapper.Seed(seed);
 
       var ballots = new List<Ballot>();
-      for (int i = 0; i < 10; i++)
+      for (int i = 0; i < _numberOfBallotsInDraw; i++)
       {
-        int ballotNumber = random.Next(10000, 99999);
+        int ballotNumber = _randomWrapper.Next(10000, 99999);
 
         var ballot = new Ballot
         {
